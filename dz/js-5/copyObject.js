687e5e5b81b76f682e7b4419fed2dcb92aae4c74
Rus <bmatsur@yahoo.com>
2024-09-21 14:25:26 +0500

    copy Objects using spread(NOT creating a link)

--- dz/js-5/copyObject.js
+++ dz/js-5/copyObject.js
@@ -8,8 +8,17 @@ const copyObject = (copyFrom) => {
 const original = { name: "Alice", age: 30 };
 const copy = copyObject(original);
 
-console.log(copy);
+console.log("original:", original);
+
+console.log("copy:", copy);
 // Ожидаемый вывод: { name: "Alice", age: 30 }
 
 console.log(original === copy);
 // Ожидаемый вывод: false (копия не является той же самой ссылкой)
+
+const copyObject2 = (copyFrom) => {
+  return { ...copyFrom }; //spread is NOT creating a link
+};
+const copy2 = copyObject2(original);
+copy2.hobby = "football";
+console.log("usig spread copy2:", copy2);

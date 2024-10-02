d89108915cd686d4ac15904bb08fe1ffc66b8aa3
Rus <bmatsur@yahoo.com>
2024-10-02 22:09:43 +0500

    js-7 done

--- dz/js-7/сash-register.js
+++ dz/js-7/сash-register.js
@@ -2,12 +2,54 @@
 console.log(" ");
 console.log("-----------сash-register-----------");
 
-
 class CashRegister {
-    items массив, который будет хранить объекты товаров. Каждый товар должен иметь следующие свойства: `name` (название товара), `price` (цена товара), `quantity` (количество).
-    - **Методы**:
-      addItem(name, price, quantity) добавляет товар в кассу.
-      removeItem(name) удаляет товар из кассы.
-      calculateTotal() вычисляет общую сумму товаров в кассе, используя метод `reduce`.
-      getReceipt() возвращает строку с информацией о всех товарах в кассе и общей сумме.
+  constructor() {
+    this.items = [];
+  }
+
+  //**Методы**:
+  addItem(name, price, quantity) {
+    this.items.push({ name, price, quantity });
+  }
+
+  removeItem(name) {
+    this.items = this.items.filter((item) => item.name !== name);
+  }
+
+  calculateTotal() {
+    return this.items.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  }
+
+  getReceipt() {
+    const itemsList = this.items
+      .map(
+        (item) =>
+          `${item.quantity} x ${item.name} - $${item.price * item.quantity}`
+      )
+      .join("\n");
+    const total = this.calculateTotal();
+    return `
+    ---- Receipt ----
+    ${itemsList}
+    -----------------
+    Sum: $${total}
+    `;
+  }
 }
+
+// Пример использования класса CashRegister
+const cashRegister = new CashRegister();
+
+// Добавляем товары
+cashRegister.addItem("Яблоки", 1.5, 3);
+cashRegister.addItem("Бананы", 1.2, 5);
+cashRegister.addItem("Апельсины", 2.0, 2);
+
+// Удаляем товар
+cashRegister.removeItem("Бананы");
+
+// Выводим чек
+console.log(cashRegister.getReceipt());

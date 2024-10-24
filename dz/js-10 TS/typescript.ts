93da24c15bee230b4a64b7d8126f44d1074eeab1
Rus <bmatsur@yahoo.com>
2024-10-24 21:03:23 +0500

    js-10 added

---
+++ dz/js-10 TS/typescript.ts
@@ -0,0 +1,13 @@
+let value: any;
+
+value = "Hello, world";
+console.log(value);
+value.toUpperCase;
+
+value = 42;
+console.log(value.toUpperCase());
+
+const x = function () {};
+let person: [string, any] = ["Alice", x];
+
+type Status = "success" | "error" | "loading";

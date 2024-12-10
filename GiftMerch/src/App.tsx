d155bb540ac49d84f54dd99551fc07bfc364b548
Rustam <bmatsur@yahoo.com>
2024-12-10 16:08:11 +0500

    useRef Hook created

--- GiftMerch/src/App.tsx
+++ GiftMerch/src/App.tsx
@@ -1,22 +1,11 @@
-// import FAQ from "./components/information/Info"
-import { ThemeContextProvider } from "./components/context/ThemeContext"
-import Box from "./components/context/Box"
 
 
 function App() {
 
   return (
     <div >
-      <div >
-        <ThemeContextProvider>
-          <Box />
-        </ThemeContextProvider>
-      </div>
     </div>
-
-    
-
-    )
+  )
 }
 
 export default App
---
+++ GiftMerch/src/components/ref/DomRef.tsx
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react'
+
+function DomRef() {
+
+    const inputRef = useRef<HTMLInputElement>(null!)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
+
+    return (
+        <div>
+            <input type="text" ref={inputRef}/>
+        </div>
+    )
+}
+
+export default DomRef
